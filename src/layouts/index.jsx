1216c234b8f3ced230e103b471244757ce994613
TheFastCat <thefastcat@hotmail.com>
2018-05-26 15:33:56 -0600

    adding SEO keywords

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,7 +8,9 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Domestic Mercenaries Blog" />
+        <Helmet defaultTitle="Domestic Mercenaries Blog" meta={[
+              { name: 'keywords', content: 'Domestic Mercenaries, home services, time management, save time, home cleaning' }
+            ]} />
         {children()}
       </div>
     );
